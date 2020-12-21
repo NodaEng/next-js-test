2531c3a20c14640ae3a14e7d721edd3a9bf6553e
ymnoda <ymnoda.eng@gmail.com>
2020-12-21 14:17:08 -0300

    link

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
     return (
         <div>
-            <h3>Home</h3>
-            <Contador />
-            <div>Teste</div>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
+            <h1>Home</h1>
+            <Link href="/sobre">
+                <a >Sobre</a>
+            </Link>
         </div>
     )
 }
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+function Sobre() {
+    return (
+        <div>
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </div>
+    )
+}
+
+export default Sobre
